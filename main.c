ae6c52dc811c98b16ddd24ce28caeb9a8f5d2be4
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-04-03 16:40:28 +0200

    Add first steps of cards and deck

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
 void printBoard();
+struct card {
+    char suit;
+    int rank;
+    int *next;
+};
+void constructDeck(int suit[], int rank[]);
 
 int main(void) {
+    //This might be better done elsewhere or maybe as a char to better
+    // represent suits, jacks, queens and kings?
 
+    // 1=C, 2=D, 3=H, 4=S
+    int suit[] = {1,2,3,4};
+
+    int rank[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+
+    constructDeck(suit, rank);
 
     printBoard();
     return 0;
 }
 
-//Early version of a method to print the board
+
+//Early version of a function to print the board
 // When variable height is implemented, the standard should be 10,
 // and increasing to the max height of a column
 void printBoard() {
@@ -33,4 +48,16 @@ void printBoard() {
         }
         printf("\n");
     }
+}
+
+void constructDeck(int suit, int rank) {
+    int suits = 4;
+    int ranks = 13;
+
+    for (int i = 0; i < suits; suit++) {
+        for (int j = 0; j < ranks; j++) {
+
+        }
+    }
+
 }
