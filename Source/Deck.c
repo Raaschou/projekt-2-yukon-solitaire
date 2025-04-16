5e1406cf3b5ea31cae96ca0cbfea1354050ff880
Mesterbob06 <s233965@student.dtu.dk>
2025-04-16 14:20:10 +0200

    Vi prøver linked

--- Source/Deck.c
+++ Source/Deck.c
@@ -1,7 +1,8 @@
 //
 // Created by August Frandsen on 15/04/2025.
 //
-
+#include "stdlib.h"
+#include "time.h"
 #include "../Include/Deck.h"
 #include "../Include/Card.h"
 #include "../Include/LinkedLists.h"
@@ -24,5 +25,71 @@ void startDeck(LinkedList *deck) {
 
             addCard(deck, newCard);
         }
+    }
+}
+
+void copyCardsFromList(LinkedList *deck, Card card[]) {
+    CardNode *current = deck->head;
+    int i = 0;
+    while (current && i < deck->size) {
+        card[i++] = current->card;
+        current = current->next;
+    }
+}
+
+void clearList(LinkedList *deck) {
+    if (!deck) return;
+
+    CardNode *current = deck->head;
+    while (current) {
+        CardNode *next = current->next;
+        free(current);
+        current = next;
+    }
+    deck->head = NULL;
+    deck->tail = NULL;
+    deck->size = 0;
+}
+
+void randomShuffle(LinkedList *deck) {
+    if (!deck || deck->size != 52) return;
+
+    Card cards[52];
+    copyCardsFromList(deck, cards);
+
+    for (int i = 51; i > 0; i--) {
+        int j = rand() % (i + 1);
+        Card temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+
+    clearList(deck);
+
+    for (int i = 0; i < 52; i++) {
+        addCard(deck, cards[i]);
+    }
+}
+
+void split_shuffle(LinkedList *deck, int cutSize) {
+    if (!deck || cutSize <= 0 || cutSize >= deck->size) return;
+
+    Card cards[52];
+    copyCardsFromList(deck, cards);
+    clearList(deck);
+
+    int leftIndex = -1;
+    int rightIndex = cutSize;
+
+    while (leftIndex < cutSize && rightIndex < 52) {
+        addCard(deck, cards[++leftIndex]);
+        addCard(deck, cards[rightIndex++]);
+    }
+
+    while (leftIndex < cutSize) {
+        addCard(deck, cards[++leftIndex]);
+    }
+    while (rightIndex < 52) {
+        addCard(deck, cards[rightIndex++]);
     }
 }
