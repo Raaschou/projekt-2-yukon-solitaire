70e2ebcb751668ebc622ac3863da03fd4371ca30
Mesterbob06 <s233965@student.dtu.dk>
2025-05-01 21:20:08 +0200

    Opdateret

--- Source/Board.c
+++ Source/Board.c
@@ -51,15 +51,14 @@ void printBoardPlayPhase(Board *board, const char *lastCommand, const char *mess
     printf("INPUT >  ");
 }
 
-//Bør printe boardet
-// håber den kan bruges til alt
+
 void printBoardStartUpPhase(Board *board, const char *lastCommand, const char *message) {
     printf("\nC1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
 
     CardNode *current = board->deck.head;
     int i = 0;
     int row = 0;
-
+if (board->deck.size!=0) {
     while (current) {
         printCard(current, &board->deck);
         current = current->next;
@@ -81,6 +80,24 @@ void printBoardStartUpPhase(Board *board, const char *lastCommand, const char *m
             row++;
         }
     }
+}else {
+    while (row < 8) {
+        printf("\t\t\t\t\t\t");
+        printf("\t\t");
+        if (row < 8 && row % 2 == 0) {
+            int f = row / 2;
+            if (board->foundations[f].head == NULL) {
+                printf("[] F%d", f + 1);
+            } else {
+                CardNode *fCard = board->foundations[f].tail;
+                printCard(fCard, &board->foundations[f]);
+            }
+        }
+        printf("\n");
+        row++;
+    }
+}
+
 
     printf("\nLast Command: %s\n", lastCommand ? lastCommand : "");
     printf("Message: %s\n", message ? message : "");
